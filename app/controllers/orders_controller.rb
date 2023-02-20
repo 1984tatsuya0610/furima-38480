526949718636d0e35440827d045fedd97ca1f1b0
1984tatsuya0610 <cloudblack793@gmail.com>
2023-02-20 14:56:05 +0900

    orders_controllerの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,7 +20,7 @@ class OrdersController < ApplicationController
 private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :sender_id, :municipality, :house_number, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :sender_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
