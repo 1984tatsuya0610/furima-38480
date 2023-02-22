07b65831cb58eef447922dd850d2dff5b2c6985f
1984tatsuya0610 <cloudblack793@gmail.com>
2023-02-22 10:24:16 +0900

    修正箇所の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,9 +10,9 @@ class OrderAddress
     validates :house_number
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :token
+    validates :sender_id,          numericality: { other_than: 1, message: "can't be blank"}
   end
-  validates :sender_id,          numericality: { other_than: 1, message: "can't be blank"}
-
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, sender_id: sender_id, municipality: municipality, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe OrderAddress, type: :model do
     before do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+      @order_address = FactoryBot.build(:order_address, user_id: user, item_id: item)
+      sleep 0.15
     end
 
     context '内容に問題ない場合' do
